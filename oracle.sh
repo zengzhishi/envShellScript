93d9a0f2a2967c0aee89ce2bb62a87ad9c7129c3
zengzhishi <zudaojun@outlook.com>
2016-11-21 17:49:34 +0800

    Complete oracle config file

--- oracle.sh
+++ oracle.sh
@@ -42,3 +42,231 @@ grep oinstall /etc/group
 #To determine whether the oraInstall.loc file exists.
 cat /etc/oraInst.loc
 ####################################################################################
+
+##### install essential dependance #######
+yum install -y binutils \
+compat-libstdc++-33 \
+elfutils-libelf \
+elfutils-libelf-devel \
+gcc \
+gcc-c++ \
+glibc \
+glibc-common \
+glibc-devel \
+glibc-headers \
+ksh \
+libaio \
+libaio-devel \
+libgcc \
+libstdc++ \
+libstdc++-devel \
+make \
+sysstat \
+unixODBC \
+unixODBC-devel
+
+
+
+DATE=`date +%Y+%m+%d`
+mkdir /bak
+#Creating Required Operating System Groups and Users
+groupadd -g 507 oinstall
+groupadd -g 502 dba
+groupadd -g 503 oper
+groupadd -g 504 asmadmin
+groupadd -g 505 asmoper
+groupadd -g 506 asmdba
+useradd -g oinstall -G dba,asmdba,oper -d /home/oracle oracle
+useradd -g oinstall -G asmadmin,asmdba,asmoper,oper,dba grid
+id oracle
+id grid
+passwd oracle
+passwd grid
+######################################
+mkdir -p /u01/app/grid
+mkdir -p /u01/app/crs_base
+mkdir -p /u01/app/crs_home
+mkdir -p /u01/app/oracle/product/11.2.0/db_1
+chown -R oracle:oinstall /u01/app/oracle
+chown -R grid:oinstall /u01/app/crs*
+chown -R grid:oinstall /u01/app/grid
+mkdir -p /u01/app/oraInventory
+chown -R grid:oinstall /u01/app/oraInventory
+chmod -R 775 /u01/
+ls -al /u01
+######################################
+#CheckResource Limits for the Oracle Software Installation Users
+yes|cp /etc/security/limits.conf /bak/bak_limits.conf
+#Installation Owner Resource Limit Recommended Ranges
+echo "#xcl "${DATE} >> /etc/security/limits.conf
+echo "oracle soft nproc 2047" >> /etc/security/limits.conf
+echo "oracle hard nproc 16384" >> /etc/security/limits.conf
+echo "oracle soft nofile 1024" >> /etc/security/limits.conf
+echo "oracle hard nofile 65536" >> /etc/security/limits.conf
+###########
+#aio
+yes|cp /proc/sys/fs/aio-max-nr /bak/aio-max-nr
+echo > /proc/sys/fs/aio-max-nr 1048576
+###########
+#Configuring Kernel Parameters for Linux
+yes|cp /etc/sysctl.conf /bak/sysctl.conf
+echo " ########### " >> /etc/sysctl.conf
+echo "#xcl "${DATE} >> /etc/sysctl.conf
+echo "fs.aio-max-nr = 1048576" >> /etc/sysctl.conf
+echo "fs.file-max = 6815744" >> /etc/sysctl.conf
+echo "kernel.shmall = 2097152" >> /etc/sysctl.conf
+echo "kernel.shmmax = 536870912" >> /etc/sysctl.conf
+echo "kernel.shmmni = 4096" >> /etc/sysctl.conf
+echo "kernel.sem = 250 32000 100 128" >> /etc/sysctl.conf
+echo "net.ipv4.ip_local_port_range = 1024 65000" >> /etc/sysctl.conf
+echo "net.core.rmem_default=262144" >> /etc/sysctl.conf
+echo "net.core.rmem_max=262144" >> /etc/sysctl.conf
+echo "net.core.wmem_default=262144" >> /etc/sysctl.conf
+echo "net.core.wmem_max=262144" >> /etc/sysctl.conf
+/sbin/sysctl -p
+###########
+cp /etc/pam.d/login /bak/login
+#64bit
+echo "#xcl "${DATE} >> /etc/pam.d/login
+echo "session required /lib/security/pam_limits.so" >> /etc/pam.d/login
+echo "session required pam_limits.so" >> /etc/pam.d/login
+
+########################################################################################
+# Add host
+echo ${OracleServerIp}" "${OracleServerName} >> /etc/hosts
+echo "HOSTNAME="${OracleServerName} > /etc/sysconfig/network
+
+#禁用网络时间服务
+systemctl stop ntpd
+systemctl disable ntpd.service
+
+#永久关闭防火墙
+sed 
+
+systemctl stop iptables
+systemctl disable iptables
+
+
+# #环境回滚
+# ######################################
+# yes|cp /bak/bak_limits.conf /etc/security/limits.conf
+# yes|cp /bak/aio-max-nr /proc/sys/fs/aio-max-nr
+# yes|cp /bak/sysctl.conf /etc/sysctl.conf
+# /sbin/sysctl -p
+# yes|cp /bak/login /etc/pam.d/login
+# ######################################
+# userdel oinstall
+# userdel dba
+# userdel oper
+# userdel asmadmin
+# userdel asmoper
+# userdel asmdba
+# groupdel dba
+# groupdel oper
+# groupdel asmadmin
+# groupdel asmoper
+# groupdel asmdba
+# ######################################
+
+
+
+
+
+############################
+DATE=`date +%Y-%m-%d`
+env_etc_profile="/etc/profile"
+env_profile="/home/oracle/.bash_profile"
+######################################
+env_ORACLE_HOSTNAME="erpdbserver"
+env_ORACLE_OWNER="oracle"
+env_ORACLE_BASE="/u01/app/oracle"
+env_ORACLE_HOME="/product/11.2.0/db_1"
+env_ORACLE_UNQNAME="xcldb"
+env_ORACLE_SID="xcldb"
+#AMERICAN_AMERICA.AL32UTF8
+env_NLS_LANG="AMMERICAN_AMERICA.ZHS16GBK"
+######################################
+yes|cp ${env_etc_profile} ${env_etc_profile}"_bak"
+yes|cp ${env_profile} ${env_profile}"_bak"
+######################################
+echo " " >> ${env_etc_profile}
+echo "###########################" >> ${env_etc_profile}
+echo "###xcl "${DATE} >> ${env_etc_profile}
+echo "if [ \$USER = \"oracle\" ]; then" >> ${env_etc_profile}
+echo " if [ \$SHELL = \"/bin/ksh\" ]; then" >> ${env_etc_profile}
+echo " ulimit -p 16384" >> ${env_etc_profile}
+echo " ulimit -n 65536" >> ${env_etc_profile}
+echo " else" >> ${env_etc_profile}
+echo " ulimit -u 16384 -n 65536" >> ${env_etc_profile}
+echo " fi" >> ${env_etc_profile}
+echo "fi" >> ${env_etc_profile}
+echo "###########################" >> ${env_etc_profile}
+######################################
+echo "###########################" >> ${env_profile}
+echo "###xcl "${DATE} >> ${env_profile}
+echo "TMP=/tmp" >> ${env_profile}
+echo "TMPDIR=\$TMP" >> ${env_profile}
+echo "export TMP TMPDIR" >> ${env_profile}
+echo " " >> ${env_profile}
+#export ORACLE_HOSTNAME="${env_ORACLE_HOSTNAME} >> ${env_profile}
+echo "ORACLE_OWNER="${env_ORACLE_OWNER} >> ${env_profile}
+echo "ORACLE_BASE="${env_ORACLE_BASE} >> ${env_profile}
+echo "ORACLE_HOME=\$ORACLE_BASE"${env_ORACLE_HOME} >> ${env_profile}
+echo "ORACLE_UNQNAME="${env_ORACLE_UNQNAME} >> ${env_profile}
+echo "ORACLE_SID="${env_ORACLE_SID} >> ${env_profile}
+echo "export ORACLE_OWNER ORACLE_BASE ORACLE_HOME ORACLE_UNQNAME ORACLE_SID" >> ${env_profile}
+echo " " >> ${env_profile}
+echo "CLASSPATH=\$ORACLE_HOME/JRE:\$ORACLE_HOME/jlib:\$ORACLE_HOME/rdbms/jlib" >> ${env_profile}
+echo "LD_LIBRARY_PATH=\$ORACLE_HOME/lib:/lib:/usr/lib:/usr/local/lib" >> ${env_profile}
+echo "export CLASSPATH LD_LIBRARY_PATH" >> ${env_profile}
+echo " " >> ${env_profile}
+echo "ORACLE_TERM=xterm" >> ${env_profile}
+#export ORACLE_TERM=vt100
+echo "NLS_LANG="${env_NLS_LANG} >> ${env_profile}
+echo "TNS_ADMIN=\$ORACLE_HOME/network/admin" >> ${env_profile}
+#echo "SQLPATH=~/mydba/sql:\$ORACLE_HOME/sqlplus/admin" >> ${env_profile}
+#echo "export ORACLE_TERM NLS_LANG TNS_ADMIN SQLPATH" >> ${env_profile}
+echo "export ORACLE_TERM NLS_LANG TNS_ADMIN " >> ${env_profile}
+echo " " >> ${env_profile}
+echo "PATH=\$ORACLE_HOME/bin:/usr/local/bin:/bin:/usr/sbin:/usr/bin:\$PATH" >> ${env_profile}
+echo "export PATH" >> ${env_profile}
+echo " " >> ${env_profile}
+#echo "PS1='\`whoami\`@\`hostname -s\`' [\$PWD]" >> ${env_profile}
+#echo "export PS1" >> ${env_profile}
+echo "PS1='[\`whoami\`@\`hostname -s\`] :'" >> ${env_profile}
+echo " " >> ${env_profile}
+echo "umask 022" >> ${env_profile}
+echo "###########################" >> ${env_profile}
+echo ""
+
+
+
+
+# install oracle 
+################################################################################
+oracle.install.responseFileVersion=/oracle/install/rspfmt_dbinstall_response_schema_v11_2_0
+oracle.install.option=INSTALL_DB_SWONLY
+ORACLE_HOSTNAME=xclora.localdomain
+UNIX_GROUP_NAME=oinstall
+INVENTORY_LOCATION=/u01/app/oraInventory
+SELECTED_LANGUAGES=en,zh_CN,zh_TW
+ORACLE_HOME=/u01/app/oracle/product/11.2.0/db_1
+ORACLE_BASE=/u01/app/oracle
+oracle.install.db.InstallEdition=EE
+oracle.install.db.isCustomInstall=true
+oracle.install.db.customComponents=oracle.rdbms.partitioning:11.2.0.1.0,oracle.rdbms.dm:11.2.0.1.0
+oracle.install.db.DBA_GROUP=dba
+oracle.install.db.OPER_GROUP=oper
+oracle.install.db.config.starterdb.type=GENERAL_PURPOSE
+oracle.install.db.config.starterdb.memoryLimit=
+oracle.install.db.config.starterdb.memoryOption=false
+oracle.install.db.config.starterdb.installExampleSchemas=false
+oracle.install.db.config.starterdb.enableSecuritySettings=true
+oracle.install.db.config.starterdb.control=DB_CONTROL
+oracle.install.db.config.starterdb.dbcontrol.enableEmailNotification=false
+SECURITY_UPDATES_VIA_MYORACLESUPPORT=false
+DECLINE_SECURITY_UPDATES=true
+
+
+
+./runInstaller -silent -ignoreSysPrereqs -force -ignorePrereq -responseFile /home/oracle/oracle_install.rsp
---
+++ oracle_config.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+
+Present_Path=`pwd`
+
+ORACLE_PATH="/home/oracle"
+oracle_file1="linux.x64_11gR2_database_1of2.zip"
+oracle_file2="linux.x64_11gR2_database_2of2.zip"
+unzip ORACLE_PATH/oracle_file1
+unzip ORACLE_PATH/oracle_file2
+
+## install dependences
+yum install -y \
+binutils \
+compat-libstdc++-33 \
+elfutils-libelf \
+elfutils-libelf-devel \
+expat \
+gcc \
+gcc-c++ \
+glibc \
+glibc-common \
+glibc-devel \
+glibc-headers \
+ksh \
+libaio \
+libaio-devel \
+libgcc \
+libstdc++ \
+libstdc++-devel \
+make \
+sysstat \
+unixODBC \
+unixODBC-devel
+
+## create user and group
+/usr/sbin/groupadd oinstall #(建立产品清单管理组)
+/usr/sbin/groupadd dba #(建立数据库安装组)
+# /usr/sbin/groupadd asmadmin #(建立ASM管理组)
+# /usr/sbin/groupadd asmdba #(建立Grid管理组)
+/usr/sbin/useradd -g oinstall -G dba -m oracle 
+
+# ## save uid and gid for user oracle
+# uid=`id oracle|awk '{print $1}'|cut -d '=' -f 2|cut -d '(' -f 1`
+# gid=`id oracle|awk '{print $2}'|cut -d '=' -f 2|cut -d '(' -f 1`
+
+## comfigure sysctl config file.
+echo "
+fs.aio-max-nr = 1048576
+fs.file-max = 6815744
+kernel.shmall = 2097152
+kernel.shmmax = 536870912
+kernel.shmmni = 4096
+kernel.sem = 250 32000 100 128
+net.ipv4.ip_local_port_range = 9000 65500
+net.core.rmem_default = 262144
+net.core.rmem_max = 4194304
+net.core.wmem_default = 262144
+net.core.wmem_max = 1048586
+" >> /etc/sysctl.conf
+
+/sbin/sysctl -p #(设置参数立即生效)
+
+## configure security setting of oracle user
+echo "oracle    soft    nproc   2047" >> /etc/security/limits.conf
+echo "oracle    hard    nproc   16384" >> /etc/security/limits.conf
+echo "oracle    soft    nofile  1024" >> /etc/security/limits.conf
+echo "oracle    hard    nofile  65536" >> /etc/security/limits.conf
+echo "oracle    soft    stack   10240" >> /etc/security/limits.conf
+
+echo "session required /lib/security/pam_limits.so" >> /etc/pam.d/login
+echo "session    required     pam_limits.so" >> /etc/pam.d/login
+
+echo "if [ $USER = "oracle" ]; then
+        if [ $SHELL = "/bin/ksh" ]; then
+              ulimit -p 16384
+              ulimit -n 65536
+        else
+              ulimit -u 16384 -n 65536
+        fi
+fi" >> /etc/profile
+
+source /etc/profile
+
+# mkdir /etc/oratab
+# mkidr /etc/oraInst.loc
+# mkdir -p /u01/app/oracle/
+
+## create install direction
+mkdir -p /u01/app/
+chown -R oracle:oinstall /u01/app/
+chmod -R 775 /u01/app/
+
+if [ ! -f /etc/oraInst.loc ];then
+    echo "nventory_loc=/u01/app/oracle/oraInventory
+    inst_group=oinstall" >> /etc/oraInst.loc
+fi
+
+chown oracle:oinstall /etc/oraInst.loc
+chmod 664 /etc/oraInst.loc
+
+## configure oracle user profile
+echo "export ORACLE_BASE=/u01/app/oracle" >> /home/oracle/.bash_profile
+echo "export ORACLE_SID=orcl" >> /home/oracle/.bash_profile
+
+source /home/oracle/.bash_profile
+
+## copy and configure response file.
+cd /home/oracle
+mkdir etc
+cp /home/oracle/database/response/* /home/oracle/etc/
+chmod 700 /home/oracle/etc/*.rsp
+
+sed -i "s/oracle.install.option=/oracle.install.option=INSTALL_DB_SWONLY/g" /home/oracle/etc/db_install.rsp
+sed -i "s/ORACLE_HOSTNAME=/ORACLE_HOSTNAME=java-linux-test/g" /home/oracle/etc/db_install.rsp
+sed -i "s/UNIX_GROUP_NAME=/UNIX_GROUP_NAME=oinstall/g" /home/oracle/etc/db_install.rsp
+sed -i "s/INVENTORY_LOCATION=/INVENTORY_LOCATION=/u01/app/oracle/oraInventory/g" /home/oracle/etc/db_install.rsp
+sed -i "s/SELECTED_LANGUAGES=/SELECTED_LANGUAGES=en,zh_CN,zh_TW/g" /home/oracle/etc/db_install.rsp
+sed -i "s/ORACLE_HOME=/ORACLE_HOME=/u01/app/oracle/product/11.2.0/db_1/g" /home/oracle/etc/db_install.rsp
+sed -i "s/ORACLE_BASE=/ORACLE_BASE=/u01/app/oracle/g" /home/oracle/etc/db_install.rsp
+sed -i "s/oracle.install.db.InstallEdition=/oracle.install.db.InstallEdition=EE/g" /home/oracle/etc/db_install.rsp
+sed -i "s/oracle.install.db.isCustomInstall=/oracle.install.db.isCustomInstall=true/g" /home/oracle/etc/db_install.rsp
+sed -i "s/oracle.install.db.DBA_GROUP=/oracle.install.db.DBA_GROUP=dba/g" /home/oracle/etc/db_install.rsp
+sed -i "s/oracle.install.db.OPER_GROUP=/oracle.install.db.OPER_GROUP=oinstall/g" /home/oracle/etc/db_install.rsp
+sed -i "s/oracle.install.db.config.starterdb.type=/oracle.install.db.config.starterdb.type=GENERAL_PURPOSE/g" /home/oracle/etc/db_install.rsp
+sed -i "s/oracle.install.db.config.starterdb.globalDBName=/oracle.install.db.config.starterdb.globalDBName=orcl/g" /home/oracle/etc/db_install.rsp
+sed -i "s/oracle.install.db.config.starterdb.SID=/oracle.install.db.config.starterdb.SID=orcl/g" /home/oracle/etc/db_install.rsp
+sed -i "s/oracle.install.db.config.starterdb.memoryLimit=/oracle.install.db.config.starterdb.memoryLimit=512/g" /home/oracle/etc/db_install.rsp
+sed -i "s/oracle.install.db.config.starterdb.password.ALL=/oracle.install.db.config.starterdb.password.ALL=oracle/g" /home/oracle/etc/db_install.rsp
+sed -i "s/DECLINE_SECURITY_UPDATES=/DECLINE_SECURITY_UPDATES=true/g" /home/oracle/etc/db_install.rsp
+
+cd $Present_Path
+echo "Oracle configure successful."
