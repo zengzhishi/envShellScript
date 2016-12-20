7251d805514a7c4a28bfc4c1504a7e4e581ccd3c
zengzhishi <zudaojun@outlook.com>
2016-12-20 18:26:29 +0800

    添加了直接通过rpm包安装的方法

--- postgresql.sh
+++ postgresql.sh
@@ -8,8 +8,28 @@
 
 source config.sh
 
-# Install postgresql96
-yum install -y https://download.postgresql.org/pub/repos/yum/9.6/redhat/rhel-7-x86_64/pgdg-centos96-9.6-3.noarch.rpm
-yum install -y postgresql96.x86_64
+# Get postgresql96
+wget https://yum.postgresql.org/9.6/redhat/rhel-7-x86_64/postgresql96-libs-9.6.1-1PGDG.rhel7.x86_64.rpm
+wget https://yum.postgresql.org/9.6/redhat/rhel-7-x86_64/postgresql96-9.6.1-1PGDG.rhel7.x86_64.rpm
+wget https://yum.postgresql.org/9.6/redhat/rhel-7-x86_64/postgresql96-server-9.6.1-1PGDG.rhel7.x86_64.rpm
+wget https://yum.postgresql.org/9.6/redhat/rhel-7-x86_64/postgresql96-contrib-9.6.1-1PGDG.rhel7.x86_64.rpm
 
-# Configure 
+
+# Install postgresql96
+rpm -ivh postgresql96-libs-9.6.1-1PGDG.rhel7.x86_64.rpm
+rpm -ivh postgresql96-9.6.1-1PGDG.rhel7.x86_64.rpm
+rpm -ivh postgresql96-server-9.6.1-1PGDG.rhel7.x86_64.rpm
+yum install -y libxslt.x86_64
+rpm -ivh postgresql96-contrib-9.6.1-1PGDG.rhel7.x86_64.rpm
+
+# Initial the postgresql96 初始化数据库
+/usr/pgsql-9.6/bin/postgresql96-setup initdb
+
+# enable start service when domain start up
+systemctl enable postgresql-9.6.service
+systemctl start postgresql-9.6.service
+
+
+export POSTGRESPATH=/usr/pgsql-9.6/bin
+export POSTGRESQLPWD='znsh417'
+su - postgres -c 'psql -U postgres --command ALTER USER postgres WITH PASSWORD '${POSTGRESQLPWD}';'
